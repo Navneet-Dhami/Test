808c93b4a8306103bf826e75fe59b62450eb04df
Mohamed Rilah <210049037@aston.ac.uk>
2023-02-16 15:56:48 +0000

    Updated Controller

--- soundwaves/soundwaves/src/main/java/team14/soundwaves/controller/MainController.java
+++ soundwaves/soundwaves/src/main/java/team14/soundwaves/controller/MainController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(path = "/")
+// @RequestMapping(path = "/")
 public class MainController {
 
-    @GetMapping
+    @GetMapping("/")
     public String index(){
-        return "index.html";
+        return "index";
     }
 }
